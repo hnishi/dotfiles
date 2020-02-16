af3f2ad024c2bbc51fd50b8689e3aaae0b5eb86e
hnishi <nishigami2509@gmail.com>
2020-02-16 14:02:46 +0900

    Add VSCode settings

--- scripts/install.sh
+++ scripts/install.sh
@@ -61,11 +61,21 @@ while [ $# -gt 0 ];do
   shift
 done
 
+# Install VSCode settings
+PATH_VSCODE_SETTING="$HOME/Library/Application Support/Code/User/settings.json"
+if [[ -f $PATH_VSCODE_SETTING ]]
+then
+  echo \"$PATH_VSCODE_SETTING\" already exists
+  echo backing it up to \"$HOME/.gdotbackup\"
+  mv "$PATH_VSCODE_SETTING" "$HOME/.gdotbackup"
+  ln -snf "$HOME/.vscode/settings.json" "$PATH_VSCODE_SETTING"
+fi
+
 if [[ "$IS_INSTALL" = true ]];then
   link_to_homedir
   command echo ""
   command echo "#####################################################"
-  command echo -e "\e[1;36m $(basename $0) install success!!! \e[m"
+  command echo -e " $(basename $0) install success!!!"
   command echo "#####################################################"
   command echo ""
 fi
